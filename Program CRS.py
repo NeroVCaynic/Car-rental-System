b286cd684e210ed4b07e197431ce3544fdd4a513
mikealpha7 <48790639+mikealpha7@users.noreply.github.com>
2021-06-28 20:26:01 +0300

    more little tweaks heh

--- Program CRS.py
+++ Program CRS.py
@@ -4,5 +4,5 @@ while True:
     print('         ╩  ╩ ╩╩╚═╩ ╩  ╩═╝╩ ╩╝╚╝╚═╝  ')
     print('             CAR RENTAL COMPANY               ')
     print('\n Welcome to Park Lane Car Rental Company!\n\nPlease choose your option below:')
-    print('\n\t1. Book or Reserve a car\n\t2. Cancel a reservation\n\n\n0. Staff Login')
+    print('\n\t1. Book or Reserve a car\n\t2. Cancel a reservation\n\n\n0. Staff Login\n')
     sel=input('Select your option: ')
