4cafd287097aed25304663ae3c5b596b4e3789ab
mikealpha7 <48790639+mikealpha7@users.noreply.github.com>
2021-06-29 20:04:38 +0300

    used def in this phrase maybe allowed if not commit bacl
    yes

--- Program CRS.py
+++ Program CRS.py
@@ -8,43 +8,62 @@ db=sql.connect(
     database='crs')
 mycursor=db.cursor()
 
-seq=1
-while seq==1:
+def clear():
+  for _ in range(50):
+     print()
+     
+
+def logo():
     print('         ╔═╗╔═╗╦═╗╦╔═  ╦  ╔═╗╔╗╔╔═╗  ')
     print('         ╠═╝╠═╣╠╦╝╠╩╗  ║  ╠═╣║║║║╣   ')
     print('         ╩  ╩ ╩╩╚═╩ ╩  ╩═╝╩ ╩╝╚╝╚═╝  ')
     print('             CAR RENTAL COMPANY               ')
+
+def staff():
+    clear()
+    logo()
+    #login credentials
+    print('\n\n\n\n\n\n \tSTAFF LOGIN \n\n Enter staff credentials:')
+    user=input('Username: ')
+    if user=='staff':
+        pas=input('Password: ')
+        if pas=='park1':
+            while True:
+                clear()
+                logo()
+                print('\n\n\t\tWelcome Staff\n')
+                #selection menu for staff
+                print('\nChoose options below:\n1. View Customers\n2. View vehicles\n3. View billing\n4. View Booking Details')
+                print('\n5. Register returned car\n6. Generate Billing invoice' )
+                sele=input('\n Enter choice: ')
+                if sele=='1':
+                    #shows customer table
+                    clear()
+                    logo()
+                    mycursor.execute("SELECT * FROM customer")
+                    cus=mycursor.fetchall()
+                    for x in cus:
+                        print(x) 
+                    con=input('\nPress y to return back to menu or press any key to log out:  ')
+                    if con=='y':
+                        continue
+                    else:
+                        break
+                        
+                        
+        else:
+            print('\nIncorrect password.\n')
+            
+    else:
+        print('\nunknown credentials.\nPlease Try Again.\n')
+#--------------------------------------------------------------------------------------------------------      
+
+seq=1
+while seq==1:
+    logo()
     print('\n Welcome to Park Lane Car Rental Company!\n\nPlease choose your option below:')
     print('\n\t1. Book or Reserve a car\n\t2. Cancel a reservation\n\n\n0. Staff Login\n')
     sel=input('Select your option: ')
     
     if sel=='0':
-        print('\n\n\n\n\n\n \tSTAFF LOGIN \n\n Enter staff credentials:')
-        user=input('Username: ')
-        if user=='staff':
-            pas=input('Password: ')
-            if pas=='park1':
-                print('\n\n\t\tWelcome Staff\n')
-                seq1=1
-                while seq1==1:
-                    print('Choose options below:\n1. View Customers\n2. View vehicles\n3. View billing\n4. View Booking Details')
-                    print('\n5. Register returned car\n6. Generate Billing invoice' )
-                    sele=input('\n Enter choice: ')
-                    if sele=='1':
-                        mycursor.execute("SELECT * FROM customer")
-                        cus=mycursor.fetchall()
-                        #cusdf=pd.DataFrame(cus)  NOT SURE BOUT THIS
-                        print(cus)
-                        con=input('\nPress y to return back to menu or press any key to log out:  ')
-                        if con=='y':
-                            continue
-                        else:
-                            break
-                    
-            else:
-                print('\nIncorrect password.\n')
-                seq-=1
-        else:
-            print('\nunknown credentials.\nPlease Try Again.\n')
-            seq-=1
-        
+        staff()
